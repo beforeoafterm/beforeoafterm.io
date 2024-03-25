60fd93717d02cadbd9960b591faff8d293f5d00a
N <beforeoafterm@gmail.com>
2024-03-25 18:03:45 +0800

    fix: Transition all

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -5,12 +5,12 @@ import { cva, type VariantProps } from 'class-variance-authority'
 import { cn } from '@/lib/utils'
 
 const buttonVariants = cva(
-  'uppercase font-serif font-bold inline-flex items-center justify-center rounded-full text-base transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50',
+  'uppercase font-serif font-bold inline-flex items-center justify-center rounded-full text-base transition-all duration-300 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50',
   {
     variants: {
       variant: {
         default:
-          'bg-primary text-primary-foreground shadow-sm hover:opacity-90 hover:text-primary',
+          'bg-primary text-primary-foreground shadow-sm hover:opacity-90',
         destructive:
           'bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/80',
         outline:
