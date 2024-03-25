bab0e804349613a8088dceef9ffd6588f27a3f89
N <beforeoafterm@gmail.com>
2024-03-25 15:35:40 +0800

    fix: Remove footer tag
    Keep the Navbar component responsibility specific to the nav tag context.

--- components/nav.tsx
+++ components/nav.tsx
@@ -22,30 +22,26 @@ const navItems = {
 export function Navbar() {
   const pathname = usePathname()
   return (
-    <footer className="sticky bottom-0 border border-b-0 border-muted bg-background p-4 drop-shadow-2xl">
-      <nav className="fade relative flex scroll-pr-6 flex-row items-start">
-        <div className="flex flex-row justify-between">
-          {Object.entries(navItems).map(([path, { name }]) => {
-            return (
-              <Link
-                key={path}
-                href={path}
-                className={cx(
-                  'relative flex select-none px-3 py-1 align-middle text-base font-bold tracking-tighter ',
-                  {
-                    'pointer-events-none underline hover:text-foreground':
-                      pathname === path,
-                    'text-muted-foreground no-underline hover:text-foreground':
-                      pathname !== path
-                  }
-                )}
-              >
-                {name}
-              </Link>
-            )
-          })}
-        </div>
-      </nav>
-    </footer>
+    <nav className="flex flex-wrap items-start justify-center gap-4">
+      {Object.entries(navItems).map(([path, { name }]) => {
+        return (
+          <Link
+            key={path}
+            href={path}
+            className={cx(
+              'relative flex select-none px-3 py-1 align-middle text-base font-bold tracking-tighter ',
+              {
+                'pointer-events-none underline hover:text-foreground':
+                  pathname === path,
+                'text-muted-foreground no-underline hover:text-foreground':
+                  pathname !== path
+              }
+            )}
+          >
+            {name}
+          </Link>
+        )
+      })}
+    </nav>
   )
 }
