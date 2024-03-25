b16884cb2fe7659b278102b84d7b9e3a55a8d61e
N <beforeoafterm@gmail.com>
2024-03-25 16:27:58 +0800

    fix: Add navbar on desktop header

--- components/layout-header.tsx
+++ components/layout-header.tsx
@@ -1,20 +1,26 @@
 import { GitHubLogoIcon, LinkedInLogoIcon } from '@radix-ui/react-icons'
 import XLogoIcon from '@/components/icons/x-logo-icon'
 import Link from 'next/link'
+import { Navbar } from './nav'
 
 export function LayoutHeader() {
   return (
     <header>
       <div>
         <Link href="/" className="no-underline hover:text-primary">
-          <h1 className="mb-2 font-slabSerif font-bold">Ronneil Petterson</h1>
+          <h1 className="mb-2 font-slabSerif font-bold lg:text-4xl">
+            Ronneil Petterson
+          </h1>
         </Link>
-        <h2 className="mb-4 font-serif font-bold">
+        <h2 className="mb-4 font-serif font-bold lg:text-xl">
           Software engineer &amp; mentor
         </h2>
-        <p className="text-muted-foreground">
+        <p className="text-muted-foreground lg:text-lg">
           I solve technical problems with user-centered solutions.
         </p>
+      </div>
+      <div className="flex-grow">
+        <Navbar />
       </div>
       <div className="my-8 flex gap-6">
         <a href="https://github.com/beforeoafterm" target="_blank">
--- components/nav.tsx
+++ components/nav.tsx
@@ -13,26 +13,20 @@ const navItems = {
   },
   '/work': {
     name: 'work'
-  },
-  '/blog': {
-    name: 'blog'
-  },
-  '/meet': {
-    name: 'meet me'
   }
 }
 
 export function Navbar() {
   const pathname = usePathname()
   return (
-    <nav className="flex flex-wrap items-start justify-center gap-4">
+    <nav className="hidden lg:my-12 lg:flex lg:flex-col lg:gap-2">
       {Object.entries(navItems).map(([path, { name }]) => {
         return (
           <Link
             key={path}
             href={path}
             className={cx(
-              'relative flex select-none px-3 py-1 align-middle text-base font-bold tracking-tighter ',
+              'relative flex w-fit select-none py-1 align-middle font-serif text-xl font-bold tracking-tighter',
               {
                 'pointer-events-none underline hover:text-foreground':
                   pathname === path,
